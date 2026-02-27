ec1330b88b9e4eb4332f803789676c4813388a16
Daniel07 <kitntnogt@gmail.com>
2026-02-27 21:03:55 +0600

    improve

--- src/api/students.js
+++ src/api/students.js
@@ -1,7 +1,7 @@
 import { apiClient } from './apiClient';
 
 export async function getStudents() {
-    const res = await apiClient.get('/api/v1/students/organizations/',{
+    const res = await apiClient.get('/api/v1/students/',{
         timeout: 90_000,
     });
     return res.data;
