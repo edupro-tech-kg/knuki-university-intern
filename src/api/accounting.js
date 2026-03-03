03523a08321f548de71abb220cf68496bd42d2eb
Ваше Имя <ady1ova178gmail.com>
2026-03-03 16:33:24 +0600

    added Accounting

--- src/api/accounting.js
+++ src/api/accounting.js
@@ -1,13 +1,3 @@
-// export const getAccounting = async () => {
-//   try {
-//     const res = await fetch("/api/v1/accounting/pricing");
-//     const data = await res.json();
-//     return data;
-//   } catch (error) {
-//     console.error("Ошибка API:", error);
-//     return [];
-//   }
-// };
 import { apiClient } from './apiClient'
 
 export async function getAccounting() {
